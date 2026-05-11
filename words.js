97f4223648881bd408a71403fd4ec332ca9c7ecf
PoppaQ <the.javabean@gmail.com>
2026-05-10 20:52:06 -0700

    add new words and resize images

--- words.js
+++ words.js
@@ -22,7 +22,8 @@
  * ========================================================================== */
 
 const WORDS = [
-  // --- Cutlery & dishes ---
+  // --- Cutlery, dishes & cleaning ---
+  { image: "images/trastes.jpg",    answer: "los trastes" },
   { image: "images/cuchillo.jpg",  answer: "el cuchillo" },
   { image: "images/tenedor.jpg",   answer: "el tenedor" },
   { image: "images/cuchara.jpg",   answer: "la cuchara" },
@@ -61,7 +62,14 @@ const WORDS = [
   { image: "images/champiñon.jpg",    answer: "el champiñon",
     alternates: ["el hongo"] },
   { image: "images/batidora.jpg",    answer: "la batidora",
-    alternates: ["la licuadora"] },   
+    alternates: ["la licuadora"] },
+  { image: "images/cebolla.jpg",     answer: "la cebolla" },
+  { image: "images/chorizo.jpg",     answer: "el chorizo" },
+  { image: "images/jamon.jpg",       answer: "el jamón" },
+  { image: "images/oliva.jpg",       answer: "la oliva",
+    alternates: ["la aceituna"] },
+  { image: "images/patata.jpg",      answer: "la patata",
+    alternates: ["la papa"] },
   
 
   // --- Drink & pantry ---
@@ -70,6 +78,8 @@ const WORDS = [
   { image: "images/azucar.jpg",    answer: "el azúcar" },
   { image: "images/cafe.jpg",      answer: "el café" },
   { image: "images/aceite.jpg",    answer: "el aceite" },
+  { image: "images/aceite-de-oliva.jpg", answer: "el aceite de oliva" },
+  { image: "images/vinagre.jpg",   answer: "el vinagre" },
   { image: "images/botella.jpg",   answer: "la botella" },
 
   // --- Actions ---
